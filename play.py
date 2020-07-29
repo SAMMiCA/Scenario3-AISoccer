c96267177cb2d50a906f990b8e38831c830d8da8
Chansol Hong <23516823+chshong@users.noreply.github.com>
2020-07-29 10:15:52 +0900

    fix reward problem and separate configs

--- play.py
+++ play.py
@@ -17,7 +17,7 @@ from sammica import perceptionSystem, reasoningSystem, learningSystem
 from sammica import action2speed
 from sammica.rewards import get_reward
 
-import argparse
+from config import parse_args
 import numpy as np
 import tensorflow as tf
 import math
@@ -65,52 +65,6 @@ ACTIVE = 4
 TOUCH = 5
 BALL_POSSESSION = 6
 
-def parse_args():
-    parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
-    # Using CPU or GPU
-    parser.add_argument("--use-cpu", action="store_true", default=False)
-    # Output Filename
-    parser.add_argument("--commit_num", type=str, default="0", help="commit number?")
-    # Environment
-    # parser.add_argument("--scenario", type=str, default="simple_", help="name of the scenario script")
-    parser.add_argument("--max-episode-len", type=int, default=500, help="maximum episode length")
-    parser.add_argument("--num-episodes", type=int, default=60000, help="number of episodes")
-    parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
-    parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
-    parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
-    # Core training parameters
-    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate for Adam optimizer")
-    parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
-    parser.add_argument("--batch-size", type=int, default=32, help="number of episodes to optimize at the same time")
-    parser.add_argument("--seed", type=int, default=1, help="number of episodes to optimize at the same time")
-    parser.add_argument("--num-units", type=int, default=256, help="number of units in the mlp")
-    # parser.add_argument("--update-freq", type=int, default=100, help="number of timesteps trainer should be updated ")
-    parser.add_argument("--no-comm", action="store_true", default=False) # for analysis purposes
-    parser.add_argument("--critic-lstm", action="store_true", default=False)
-    parser.add_argument("--actor-lstm", action="store_true", default=False)
-    parser.add_argument("--centralized-actor", action="store_true", default=False)
-    parser.add_argument("--with-comm-budget", action="store_true", default=False)
-    parser.add_argument("--analysis", type=str, default="", help="type of analysis") # time, pos, argmax
-    parser.add_argument("--commit-num", type=str, default="", help="name of the experiment")
-    parser.add_argument("--sync-sampling", action="store_true", default=False)
-    parser.add_argument("--tracking", action="store_true", default=True)
-    # Checkpointing
-    parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
-    parser.add_argument("--save-dir", type=str, default="../../examples/ai28_player/saved_policy/", help="directory in which training state and model should be saved")
-    parser.add_argument("--save-rate", type=int, default=100, help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="./saved_policy/", help="directory in which training state and model are loaded")
-    parser.add_argument("--test-actor-q", action="store_true", default=False)
-    # Evaluation
-    parser.add_argument("--graph", action="store_true", default=True)
-    parser.add_argument("--restore", action="store_true", default=False)
-    parser.add_argument("--metrics-filename", type=str, default="", help="name of metrics filename")
-    parser.add_argument("--display", action="store_true", default=False)
-    parser.add_argument("--benchmark", action="store_true", default=False)
-    parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
-    parser.add_argument("--benchmark-dir", type=str, default="../../examples/ai28_player/benchmark_files/", help="directory where benchmark data is saved")
-    parser.add_argument("--plots-dir", type=str, default="../../examples/ai28_player/learning_curves/", help="directory where plot data is saved")
-    arglist, _ = parser.parse_known_args()
-    return arglist
 
 def get_lstm_states(_type, trainers):
     if _type == 'p':
@@ -378,7 +332,8 @@ class player(Participant):
 
         if TRAINING :
             if len(self.replayBuffer.frame_buffer) > 0 :
-                rew_n = get_reward(self.info, frame, self.replayBuffer)
+                rew_agent = get_reward(self.info, frame, self.replayBuffer)
+                rew_n = np.array([sum(rew_agent)/5 for i in range(self.num_agent)])
                 self.replayBuffer.reward_buffer.push(rew_n)
             self.replayBuffer.frame_buffer.push(frame)
 
@@ -438,10 +393,10 @@ class player(Participant):
             # Adding rewards
             if arglist.tracking:
                 for i, a in enumerate(self.trainers):
-                    a.tracker.record_information("ag_reward", rew_n[i])
+                    a.tracker.record_information("ag_reward", rew_agent[i])
 
-            for i, rew in enumerate(rew_n):
-                self.episode_rewards[-1] += rew
+            for i, rew in enumerate(rew_agent):
+                self.episode_rewards[-1] += rew/self.num_agent
                 self.agent_rewards[i][-1] += rew
 
             # If an episode was finished, reset internal values
